930381baec442c159f69c77b744f07d3a4d2a077
nazrinshukurova <nezrinnshukurova23>
2025-02-10 20:23:45 +0400

    Add footer

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "./footer.css";
+import pizzaSvg from "../../public/pizza.svg";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top-footer">
+        <p className="textReservations">Reservations and telephone orders</p>
+        <div className="phoneBox">
+          <p className="phoneNumber">699.335.4887-699.335.4886</p>
+        </div>
+      </div>
+      <div className="down-part">
+        <img src={pizzaSvg} alt="Pizza Icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header/Header";
 import Data from "./components/data/Data";
+import Footer from "./Footer/Footer";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
     <Data />
+    <Footer/>
   </>
 );
